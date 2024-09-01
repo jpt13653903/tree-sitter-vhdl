40990deec8f07514d87c422171d083ec7d62e4d2
John-Philip Taylor <jpt13653903@gmail.com>
2024-09-01 09:45:19 +0200

    Updated Node and regenerated

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -3,13 +3,13 @@ package tree_sitter_vhdl_test
 import (
 	"testing"
 
-	tree_sitter "github.com/smacker/go-tree-sitter"
-	"github.com/jpt13653903/tree-sitter-vhdl"
+	tree_sitter "github.com/tree-sitter/go-tree-sitter"
+	tree_sitter_vhdl "github.com/tree-sitter/tree-sitter-vhdl/bindings/go"
 )
 
 func TestCanLoadGrammar(t *testing.T) {
 	language := tree_sitter.NewLanguage(tree_sitter_vhdl.Language())
 	if language == nil {
-		t.Errorf("Error loading VHDL grammar")
+		t.Errorf("Error loading Vhdl grammar")
 	}
 }
